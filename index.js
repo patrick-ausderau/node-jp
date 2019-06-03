7e9ca48b598865b0096ad45a45c9dd88e5137a97
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2019-06-03 13:50:23 +0900

    configuration with .env file

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 'use strict';
 
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World from Patrick');
 });
 
 app.get('/test', (req, res) => {
   res.send('Testing is fun');
 });
 
-app.listen(3000);
+app.listen(process.env.PORT);
