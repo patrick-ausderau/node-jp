6cc4b631434fe17b5a6c66fecc6451f6a1c3aa92
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2019-07-01 14:31:06 +0900

    passport authentication in progress

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const db = require('./db');
+
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    if (username !== process.env.username || password !== process.env.password) {
+      done(null, false, {message: 'Incorrect credentials.'});
+      return;
+    }
+    return done(null, {username: username}); // returned object usally contains something to identify the user
+  }
+));
+
+const app = express();
+app.use(express.static('public'));
+app.use(passport.initialize());
 
 db.on('connected', () => {
   if(process.env.NODE_ENV === 'development') {
@@ -13,12 +29,17 @@ db.on('connected', () => {
   }
 });
 
-const app = express();
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.use('/user', require('./user/routes'));
+
+app.post('/login', 
+  passport.authenticate('local', { 
+    successRedirect: '/', 
+    failureRedirect: '/login.html', 
+    session: false })
+);
 
 app.get('/', (req, res) => {
   if(req.secure) {
